29bea9ba549ffb1622a7f205ecb36dc8915babae
yamashin0922 <y@zenryokukikai.com>
2023-04-13 05:08:18 +0900

    Add Integer Programing to the animation

--- src/animation.py
+++ src/animation.py
@@ -2,23 +2,26 @@
 import matplotlib.pyplot as plt
 from matplotlib import animation, rc
 class Animation:
-  def __init__(self, classic, qa, frame=800):
-    self.classic = classic
+  def __init__(self, greedy, ip, qa, frame=400):
+    self.greedy = greedy
+    self.ip = ip
     self.qa = qa
-    self.fig = plt.figure(figsize=(22, 10))
+    self.fig = plt.figure(figsize=(33, 10))
 
-    grid = plt.GridSpec(1, 2, wspace=0.4, hspace=0.3)
+    grid = plt.GridSpec(1, 3, wspace=0.4, hspace=0.3)
     self.ax1 = plt.subplot(grid[0, 0])
     self.ax2 = plt.subplot(grid[0, 1])
+    self.ax3 = plt.subplot(grid[0, 2])
     self.frame = frame
 
   def initPlot(self):
-    return self.classic.initPlot(self.ax1,'Cassic') + self.qa.initPlot(self.ax2,'Quantum annealing')
+    return self.greedy.initPlot(self.ax1,'Greedy') + self.ip.initPlot(self.ax2,'Integer Programming') + self.qa.initPlot(self.ax3,'Quantum annealing')
 
   def update(self, i):
-    return self.classic.update(i) + self.qa.update(i)
+    return self.greedy.update(i) + self.ip.update(i) + self.qa.update(i) 
 
   def animate(self):
     anim = animation.FuncAnimation(self.fig, self.update, init_func=self.initPlot, frames=self.frame, interval=100, blit=True)
-    rc('animation', html='jshtml')
+    #anim._func_cid = self.fig.canvas.mpl_connect('resize_event', anim._resize)
+    #rc('animation', html='jshtml')
     return anim
--- src/doctor_helis.py
+++ src/doctor_helis.py
@@ -1,20 +1,45 @@
 # -*- coding: utf-8 -*-
 import optimizer
-import animation
+import animation as ani
+from matplotlib import animation
 
 def main():
     num_of_fire_departments = 9 #救急車
     num_of_basehospitals = 8 #ドクターヘリ
     width = 20000
     height = 20000
+    animes = []
+    params = []
 
+    cnt = 0
     for num_of_patients in [8,]:
       for num_of_rendezvous_points in [11,]:
-        df, best_world_classic, best_world_ip, best_world_qa = optimizer.evaluate(num_of_patients = num_of_patients, width = width, height = height, num_of_fire_departments=num_of_fire_departments, num_of_rendezvous_points=num_of_rendezvous_points, num_of_basehospitals=num_of_basehospitals, map_relocations=10, qa_trial_count=3, use_d_wave=True, is_new_algorithm_p1 = True, is_new_algorithm_p2 = True)
+        map_relocations=10
+        qa_trial_count=3
+        params.append([cnt,width,height,num_of_fire_departments,num_of_patients,\
+                       num_of_rendezvous_points,num_of_basehospitals,\
+                       map_relocations,qa_trial_count])  
+        cnt += 1
+        df, best_world_classic, best_world_ip, best_world_qa = optimizer.evaluate(num_of_patients = num_of_patients,\
+                width = width, height = height, num_of_fire_departments=num_of_fire_departments,\
+                num_of_rendezvous_points=num_of_rendezvous_points, num_of_basehospitals=num_of_basehospitals,\
+                map_relocations=map_relocations, qa_trial_count=qa_trial_count, use_d_wave=True,\
+                is_new_algorithm_p1 = True, is_new_algorithm_p2 = True)
+
         print(df)
 
-        #anim = animation.Animation(best_world_classic, best_world_qa, 2000).animate()
-        #anim
+        anim = ani.Animation(best_world_classic, best_world_ip, best_world_qa, 1000).animate()
+        animes.append(anim)
+
+    for anime, params in zip(animes,params):
+        video_filename = '{:02}'.format(params[0]) + '_' + 'w' + str(params[1]) + '_' + 'h' + str(params[2]) + \
+                    '_' +'a' + str(params[3]) + '_' + 'p' + str(params[4]) + \
+                    '_' +'r' + str(params[5]) + '_' + 'd' + str(params[6]) + \
+                    '_' +'m' + str(params[7]) + '_' + 'q' + str(params[8]) + '.avi'
+        print(video_filename)
+        writervideo = animation.FFMpegWriter(fps=30)
+        anime.save(video_filename, writer=writervideo)
+        print('Done')
 
 if __name__ == "__main__":
     main()
